ea5747d98a72c90bed38501fc8e8828101272167
Paulina Cole <lina.cole@gmail.com>
2022-09-13 19:18:39 -0400

    updating landing page

--- client/index.js
+++ client/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './App'
+import './index.css';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
   <Provider store={store}>
     <Router history={history}>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+  </Provider>
+);
