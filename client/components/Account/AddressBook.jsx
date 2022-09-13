b5348d72237226d0436c3ac6c1ccba9bd80a09c7
Paulina Cole <lina.cole@gmail.com>
2022-09-12 22:48:28 -0400

    intial component + store structure

---
+++ client/components/Account/AddressBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddressBook = () => {
+  return (
+    <div>AddressBook</div>
+  )
+}
---
+++ client/components/Admin/AdminDish.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdminDish = () => {
+  return (
+    <div>AdminDish</div>
+  )
+}
