1ada30c55372a92164780a619b2f73f0a6d0ebaf
Paulina Cole <lina.cole@gmail.com>
2022-09-15 20:49:47 -0400

    working on cart

--- client/components/Pages/Header.js
+++ client/components/Pages/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Search } from './Search'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
 
 export const Header = () => {
   return (
     <div>
         <Search />
+      <div className='flex mf: flex-row flex-col items-end justify-between'>
+        <AiOutlineShoppingCart className="text-[30px]" />
+      </div>
     </div>
   )
 }
